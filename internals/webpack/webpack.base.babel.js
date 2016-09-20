b56a901c100b4a843e4f4a6c92f3862f0dc2fbe5
Sahat Yalkabov <sakhat@gmail.com>
2016-09-19 17:52:09 -0700

    removed internals directory

--- internals/webpack/webpack.base.babel.js
+++ internals/webpack/webpack.base.babel.js
@@ -12,43 +12,47 @@ module.exports = (options) => ({
     publicPath: '/',
   }, options.output), // Merge with env dependent settings
   module: {
-    loaders: [{
-      test: /\.js$/, // Transform all .js files required somewhere with Babel
-      loader: 'babel',
-      exclude: /node_modules/,
-      query: options.babelQuery,
-    }, {
-      // Transform our own .css files with PostCSS and CSS-modules
-      test: /\.css$/,
-      exclude: /node_modules/,
-      loader: options.cssLoaders,
-    }, {
-      // Do not transform vendor's CSS with CSS-modules
-      // The point is that they remain in global scope.
-      // Since we require these CSS files in our JS or CSS files,
-      // they will be a part of our compilation either way.
-      // So, no need for ExtractTextPlugin here.
-      test: /\.css$/,
-      include: /node_modules/,
-      loaders: ['style-loader', 'css-loader'],
-    }, {
-      test: /\.(eot|svg|ttf|woff|woff2)$/,
-      loader: 'file-loader',
-    }, {
-      test: /\.(jpg|png|gif)$/,
-      loaders: [
-        'file-loader',
-      ],
-    }, {
-      test: /\.html$/,
-      loader: 'html-loader',
-    }, {
-      test: /\.json$/,
-      loader: 'json-loader',
-    }, {
-      test: /\.(mp4|webm)$/,
-      loader: 'url-loader?limit=10000',
-    }],
+    loaders: [
+      {
+        test: /\.js$/, // Transform all .js files required somewhere with Babel
+        loader: 'babel',
+        exclude: /node_modules/,
+        query: options.babelQuery,
+      },
+      {
+        // Transform our own .css files with PostCSS and CSS-modules
+        test: /\.css$/,
+        exclude: /node_modules/,
+        loader: options.cssLoaders,
+      },
+      {
+        // Do not transform vendor's CSS with CSS-modules
+        // The point is that they remain in global scope.
+        // Since we require these CSS files in our JS or CSS files,
+        // they will be a part of our compilation either way.
+        // So, no need for ExtractTextPlugin here.
+        test: /\.css$/,
+        include: /node_modules/,
+        loaders: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        loader: 'file-loader',
+      }, {
+        test: /\.(jpg|png|gif)$/,
+        loaders: [
+          'file-loader',
+        ],
+      }, {
+        test: /\.html$/,
+        loader: 'html-loader',
+      }, {
+        test: /\.json$/,
+        loader: 'json-loader',
+      }, {
+        test: /\.(mp4|webm)$/,
+        loader: 'url-loader?limit=10000',
+      }],
   },
   plugins: options.plugins.concat([
     new webpack.ProvidePlugin({
