11a8e04452f416175c98e8f17ee032bfc8cb9bdc
Sahat Yalkabov <sakhat@gmail.com>
2016-10-02 07:37:53 -0700

    app, routes, store refactoring

--- app/app.js
+++ app/app.js
@@ -8,7 +8,7 @@
 // Needed for redux-saga es6 generator support
 import 'babel-polyfill';
 
-// Needed for Bootstrap CSS
+// Bootstrap CSS
 import 'bootstrap/dist/js/bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 
@@ -21,8 +21,7 @@ import 'file?name=[name].[ext]!./favicon.png';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { applyRouterMiddleware, Router, browserHistory } from 'react-router';
-import { syncHistoryWithStore } from 'react-router-redux';
+import { Router, browserHistory, applyRouterMiddleware } from 'react-router';
 import useScroll from 'react-router-scroll';
 import configureStore from './store';
 
@@ -33,35 +32,15 @@ import configureStore from './store';
 const initialState = {};
 const store = configureStore(initialState, browserHistory);
 
-// If you use Redux devTools extension, since v2.0.1, they added an
-// `updateStore`, so any enhancers that change the store object
-// could be used with the devTools' store.
-// As this boilerplate uses Redux & Redux-Saga, the `updateStore` is needed
-// if you want to `take` actions in your Sagas, dispatched from devTools.
-if (window.devToolsExtension) {
-  window.devToolsExtension.updateStore(store);
-}
-
-const history = syncHistoryWithStore(browserHistory, store);
-
-// Set up the router, wrapping all Routes in the App component
-import App from 'containers/App';
+// Set up the routes
 import createRoutes from './routes';
-const rootRoute = {
-  component: App,
-  childRoutes: createRoutes(store),
-};
 
 ReactDOM.render(
   <Provider store={store}>
     <Router
-      history={history}
-      routes={rootRoute}
-      render={
-        // Scroll to top when going to a new page, imitating default browser
-        // behaviour
-        applyRouterMiddleware(useScroll())
-      }
+      history={browserHistory}
+      routes={createRoutes(store)}
+      render={applyRouterMiddleware(useScroll())}
     />
   </Provider>,
   document.getElementById('app')
--- app/store.js
+++ app/store.js
@@ -1,41 +1,22 @@
-/**
- * Create the store with asynchronously loaded reducers
- */
-
 import { createStore, applyMiddleware, compose } from 'redux';
-import { fromJS } from 'immutable';
-import { routerMiddleware } from 'react-router-redux';
-import createSagaMiddleware from 'redux-saga';
+import thunk from 'redux-thunk';
+
 import createReducer from './reducers';
 
-const sagaMiddleware = createSagaMiddleware();
 const devtools = window.devToolsExtension || (() => noop => noop);
 
-export default function configureStore(initialState = {}, history) {
-  // Create the store with two middlewares
-  // 1. sagaMiddleware: Makes redux-sagas work
-  // 2. routerMiddleware: Syncs the location/URL path to the state
-  const middlewares = [
-    sagaMiddleware,
-    routerMiddleware(history),
-  ];
-
-  const enhancers = [
-    applyMiddleware(...middlewares),
-    devtools(),
-  ];
+export default function configureStore(initialState = {}) {
+  const enhancer = compose(
+    applyMiddleware(thunk),
+    devtools()
+  );
 
   const store = createStore(
     createReducer(),
-    fromJS(initialState),
-    compose(...enhancers)
+    initialState,
+    enhancer
   );
 
-  // Extensions
-  store.runSaga = sagaMiddleware.run;
-  store.asyncReducers = {}; // Async reducer registry
-
-  // Make reducers hot reloadable, see http://mxs.is/googmo
   /* istanbul ignore next */
   if (module.hot) {
     module.hot.accept('./reducers', () => {
