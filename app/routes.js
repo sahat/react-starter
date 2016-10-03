adafa7dfa8e991141a02823f3e371d438a343193
Sahat Yalkabov <sakhat@gmail.com>
2016-10-02 18:14:41 -0700

    add new examples routes

--- app/routes.js
+++ app/routes.js
@@ -1,16 +1,26 @@
 import React from 'react';
-import { Route } from 'react-router';
+import { Route, IndexRoute } from 'react-router';
 
 import App from 'containers/App';
 import HomePage from 'containers/HomePage';
 import FeaturePage from 'containers/FeaturePage';
+import ExamplesPage from 'containers/ExamplesPage';
 import NotFoundPage from 'containers/NotFoundPage';
+
+// Example pages
+import BootstrapGlobal from 'containers/ExamplesPage/BootstrapGlobal';
+import BootstrapClassnames from 'containers/ExamplesPage/BootstrapClassnames';
 
 export default function createRoutes(store) {
   return (
     <Route component={App}>
       <Route path="/" component={HomePage} />
       <Route path="/features" component={FeaturePage} />
+      <Route path="/examples">
+        <IndexRoute component={ExamplesPage} />
+        <Route path="bootstrap/global" component={BootstrapGlobal} />
+        <Route path="bootstrap/classnames" component={BootstrapClassnames} />
+      </Route>
       <Route path="*" component={NotFoundPage} />
     </Route>
   );
